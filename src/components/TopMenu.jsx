a92a59f57796763c652ce8349dd9fc4af67e827f
Jatez95 <mateossanchezjavier@gmail.com>
2025-03-15 18:37:22 +0100

    Changed top menu styles

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -8,39 +8,45 @@ export default function TopMenu() {
 
     return (
         <>
-            <nav className='fixed top-0 h-16 w-full flex flex-row bg-black/90 border-b-white border-b-2 backdrop-blur-lg backdrop-filter z-50'>
+            <nav className='fixed top-0 h-16 w-full flex flex-row bg-black/90 border-b-neutral-600 border-b-2 backdrop-blur-lg backdrop-filter z-50'>
                 <div className="flex h-full w-1/2 lg:w-2/4 justify-start items-center">
                     <p className="text-neutral-50 m-5 text-xl">JMateos</p>
                 </div>
-                <div className="flex h-full w-1/2 lg:w-2/4 justify-end items-center mr-4 space-x-1 lg:space-x-3">
-                    <a href="#about" className="hidden lg:inline">
-                        <Button text="Sobre mi" />
+                <div className="flex h-full w-1/2 lg:w-2/4 lg:space-x-4 justify-end items-center mr-4">
+                    <a href="#about" className="hidden lg:inline lg:h-full">
+                        <Button text="Sobre mi" className='h-full w-28 text-sm text-neutral-500 uppercase font-semibold' />
                     </a>
-                    <a href="#experience" className="hidden lg:inline">
-                        <Button text="Experiencia" />
+                    <a href="#experience" className="hidden lg:inline lg:h-full">
+                        <Button text="Experiencia" className='h-full w-28 text-sm text-neutral-500 uppercase font-semibold'/>
                     </a>
-                    <a href="#projects" className="hidden lg:inline">
-                        <Button text="Proyectos" />
+                    <a href="#techs" className="hidden lg:inline lg:h-full">
+                        <Button text="Technologias" className='h-full w-28 text-sm text-neutral-500 uppercase font-semibold'/>
                     </a>
-                    <a onClick={handleCvButton} className="hidden lg:inline">
-                        <Button text="CV" />
+                    <a href="#projects" className="hidden lg:inline lg:h-full">
+                        <Button text="Proyectos" className='h-full w-28 text-sm text-neutral-500 uppercase font-semibold'/>
+                    </a>
+                    <a onClick={handleCvButton} className="hidden lg:inline lg:h-full">
+                        <Button text="CV" className='h-full w-28 text-sm text-neutral-500 uppercase font-semibold'/>
                     </a>
                     <button className="lg:hidden text-neutral-50" onClick={toggleMenu}>
                         ☰
                     </button>
                 </div>
             </nav>
-            <div id="mobile-menu" className="hidden lg:hidden fixed top-16 left-0 w-full bg-black/90 border-b-white border-b-2 backdrop-blur-lg backdrop-filter z-40">
-                <a href="#about" className="block p-4 text-neutral-50">
+            <div id="mobile-menu" className="hidden lg:hidden fixed top-16 left-0 w-full bg-black/90 border-b-neutral-500 border-b-2 backdrop-blur-lg backdrop-filter z-40">
+                <a href="#about" className="block p-4 text-neutral-400">
                     Sobre Mi
                 </a>
-                <a href="#experience" className="block p-4 text-neutral-50">
+                <a href="#experience" className="block p-4 text-neutral-400">
                     Experiencia
                 </a>
-                <a href="#projects" className="block p-4 text-neutral-50">
+                <a href="#techs" className="block p-4 text-neutral-400">
+                    Experiencia
+                </a>
+                <a href="#projects" className="block p-4 text-neutral-400">
                     Proyectos
                 </a>
-                <a href="#cv" className="block p-4 text-neutral-50" onClick={handleCvButton}>
+                <a href="#cv" className="block p-4 text-neutral-400" onClick={handleCvButton}>
                     CV
                 </a>
             </div>
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({ text }) {
+export default function Button({ text, className }) {
     return (
-        <button className="hover:opacity-100 bg-neutral-950 h-8 w-28 mt-1 ml-4 text-sm text-white hover:text-black hover:bg-neutral-50 uppercase">
+        <button className={className}>
             {text}
         </button>
     );
